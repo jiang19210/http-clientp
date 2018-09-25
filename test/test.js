11749cf69f837a9da861667ded8e80d95dfc0f10
姜光明 <jiangguangming@u51.com>
2018-09-25 15:57:27 +0800

    增加headers处理,增加headers单元测试

--- test/test.js
+++ test/test.js
@@ -1,9 +1,14 @@
 const httpClient = require('../lib/http-clientp');
 const assert = require('assert');
+var proxy_hots = '10.211.55.13';
+var port = '8888';
+var proxy_url = 'http://10.211.55.13:8888';
+var test_url = 'https://www.baidu.com/';
+
 describe('httpClient', function () {
     describe('#http#get', function () {
         it('should return 200 when in test environment', function (done) {
-            httpClient.get('https://www.baidu.com/', function (err, body, res, httpContext) {
+            httpClient.get(test_url, function (err, body, res, httpContext) {
                 assert.equal(200, res.statusCode);
                 assert.equal(200, httpContext.response.statusCode);
                 done();
@@ -13,7 +18,7 @@ describe('httpClient', function () {
     describe('#http#get#request#timeout', function () {
         it('should return 200 when in test environment', function (done) {
             var httpcontext = {'request': {'timeout': 1000}};
-            httpClient.get('https://www.baidu.com/', function (err, body, res, httpContext) {
+            httpClient.get(test_url, function (err, body, res, httpContext) {
                 assert.equal(200, res.statusCode);
                 done();
             }, httpcontext);
@@ -21,8 +26,8 @@ describe('httpClient', function () {
     });
     describe('#https#get#request#proxy#本地必须启动8888端口的代理', function () {
         it('should return 200 when in test environment', function (done) {
-            var httpcontext = {'request': {'proxy': {'host': '127.0.0.1', 'port': 8888}}};
-            httpClient.get('https://www.baidu.com/', function (err, body, res, httpContext) {
+            var httpcontext = {'request': {'proxy': {'host': proxy_hots, 'port': port}}};
+            httpClient.get(test_url, function (err, body, res, httpContext) {
                 assert.equal(200, res.statusCode);
                 done();
             }, httpcontext);
@@ -30,8 +35,8 @@ describe('httpClient', function () {
     });
     describe('#https#get#request#proxy#本地必须启动8888端口的代理', function () {
         it('should return 200 when in test environment', function (done) {
-            var httpcontext = {'request': {'http_proxy': 'http://127.0.0.1:8888'}};
-            httpClient.get('https://www.baidu.com/', function (err, body, res, httpContext) {
+            var httpcontext = {'request': {'http_proxy': proxy_url}};
+            httpClient.get(test_url, function (err, body, res, httpContext) {
                 assert.equal(200, res.statusCode);
                 done();
             }, httpcontext);
@@ -39,8 +44,8 @@ describe('httpClient', function () {
     });
     describe('#http#get#request#proxy#本地必须启动8888端口的代理', function () {
         it('should return 200 when in test environment', function (done) {
-            var httpcontext = {'request': {'proxy': {'host': '127.0.0.1', 'port': 8888}}};
-            httpClient.get('http://www.dianping.com/shanghai', function (err, body, res, httpContext) {
+            var httpcontext = {'request': {'proxy': {'host': proxy_hots, 'port': port}}};
+            httpClient.get(test_url, function (err, body, res, httpContext) {
                 assert.equal(200, res.statusCode);
                 done();
             }, httpcontext);
@@ -48,8 +53,8 @@ describe('httpClient', function () {
     });
     describe('#http#get#request#http_proxy#本地必须启动8888端口的代理', function () {
         it('should return 200 when in test environment', function (done) {
-            var httpcontext = {'request': {'http_proxy': 'http://127.0.0.1:8888'}};
-            httpClient.get('http://www.dianping.com/shanghai', function (err, body, res, httpContext) {
+            var httpcontext = {'request': {'http_proxy': proxy_url}};
+            httpClient.get(test_url, function (err, body, res, httpContext) {
                 assert.equal(200, res.statusCode);
                 done();
             }, httpcontext);
@@ -58,7 +63,7 @@ describe('httpClient', function () {
     describe('#get#httpContext#data#将请求的数据附带到结果中', function () {
         it('should return 200 when in test environment', function (done) {
             var httpcontext = httpClient.build_httpcontext('GET', {'key': 'value'});
-            httpClient.get('http://www.dianping.com/shanghai', function (err, body, res, httpContext) {
+            httpClient.get(test_url, function (err, body, res, httpContext) {
                 assert.equal(200, res.statusCode);
                 assert.equal('value', httpContext.data.key);
                 done();
@@ -67,11 +72,11 @@ describe('httpClient', function () {
     });
     describe('#request#httpContext#data', function () {
         it('should return 200 when in test environment', function (done) {
-            var httpcontext = httpClient.build_httpcontext('GET', {'key': 'value', 'url': 'http://doc.redisfans.com/'});
+            var httpcontext = httpClient.build_httpcontext('GET', {'key': 'value', 'url': test_url});
             httpClient.request(httpcontext.request.options, function (err, body, res, httpContext) {
                 assert.equal(200, res.statusCode);
                 assert.equal('value', httpContext.data.key);
-                assert.equal('http://doc.redisfans.com/', httpContext.data.url);
+                assert.equal(test_url, httpContext.data.url);
                 done();
             }, httpcontext);
         });
@@ -79,14 +84,14 @@ describe('httpClient', function () {
 
     describe('#request_select_proxy#httpContext#同步获取代理', function () {
         it('should return 200 when in test environment', function (done) {
-            var httpcontext = httpClient.build_httpcontext('GET', {'key': 'value', 'url': 'http://doc.redisfans.com/'});
+            var httpcontext = httpClient.build_httpcontext('GET', {'key': 'value', 'url': test_url});
             httpcontext.proxymodel = 'dynamic';
             httpcontext.callback = function (httpcontext) {
                 assert.equal(200, httpcontext.res.statusCode);
                 done();
             };
             httpClient.request_select_proxy(httpcontext, function (callback) {
-                var http_proxy = syncProxy();//'http://127.0.0.1:8888';//此代理可以根据函数获取,所以为可变动的动态代理
+                var http_proxy = syncProxy();//此代理可以根据函数获取,所以为可变动的动态代理
                 httpcontext.request.http_proxy = http_proxy;
                 callback(httpcontext);
             });
@@ -94,7 +99,7 @@ describe('httpClient', function () {
     });
     describe('#request_select_proxy#httpContext#异步获取代理', function () {
         it('should return 200 when in test environment', function (done) {
-            var httpcontext = httpClient.build_httpcontext('GET', {'key': 'value', 'url': 'http://doc.redisfans.com/'});
+            var httpcontext = httpClient.build_httpcontext('GET', {'key': 'value', 'url': test_url});
             httpcontext.proxymodel = 'dynamic';
             httpcontext.callback = function (httpcontext) {
                 assert.equal(200, httpcontext.res.statusCode);
@@ -108,12 +113,23 @@ describe('httpClient', function () {
             });
         });
     });
+
+    describe('#request#headers', function () {
+        it('should return 200 when in test environment', function (done) {
+            var httpContext = {'request': {'headers': {"Cookie": "BAIDUID=53339F113B44CCC39F2B520C6E660F17:FG=1"}}};
+            httpClient.get({'path': test_url, 'headers': {"Cookie": "BAIDUID=53339F113B44CCC39F2B520C6E660F17:FG=12"}}, function (err, body, res, httpContext) {
+                assert.equal(200, res.statusCode);
+                assert.equal('BAIDUID=53339F113B44CCC39F2B520C6E660F17:FG=12', httpContext.request.headers.Cookie);
+                done();
+            }, httpContext);
+        });
+    });
 });
 
 function syncProxy() {
-    return 'http://127.0.0.1:8888';
+    return proxy_url;
 }
 
 function asyncProxy(calback) {
-    calback('http://127.0.0.1:8888');
+    calback(proxy_url);
 }
